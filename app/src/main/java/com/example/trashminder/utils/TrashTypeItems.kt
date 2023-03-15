19b46e5c6aad617fa61be49d2b243bac09af27af
OnicaAdrianCatalin <>
2023-03-15 14:39:46 +0200

    Fixed the displaying metal issue and added an enum class for replacing the hardcoded strings

---
+++ app/src/main/java/com/example/trashminder/utils/TrashTypeItems.kt
@@ -0,0 +1,9 @@
+package com.example.trashminder.utils
+
+enum class TrashTypeItems(val trash: String, val type: String) {
+    Plastic("plastic", "Plastic"),
+    Metale("metale", "Metal"),
+    Hartie("hartie", "Hartie si Carton"),
+    Sticla("sticla", "Sticla"),
+    Menajer("menajer", "Menajer")
+}
